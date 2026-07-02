03e5d4c145565722bab00f6ec7a2ffd476c30530
Anton A Gelman <agelman@gmail.com>
2026-07-02 15:39:43 -0400

    Switch guide to grey orange theme

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "gelman-travel-guide-v5";
-const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "gelman-travel-guide-v6";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./app-logo.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
@@ -15,9 +15,13 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  if (event.request.mode === "navigate" || event.request.destination === "document") {
-    event.respondWith(fetch(event.request).catch(() => caches.match("./index.html")));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match("./index.html")))
+  );
 });
