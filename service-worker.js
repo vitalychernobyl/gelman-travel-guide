9ef075f2e7e438f00aafef12daced8e99c9cd239
Anton A Gelman <agelman@gmail.com>
2026-07-03 17:23:15 -0400

    Add Uber deep links to travel cards

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const APP_VERSION = "51";
+const APP_VERSION = "52";
 const CACHE = `gelman-travel-guide-v${APP_VERSION}`;
 const FILES = [
   "./",
   "./index.html",
-  "./?v=51",
+  "./?v=52",
   "./app-version.json",
-  "./app-version.json?v=51",
+  "./app-version.json?v=52",
   "./manifest.webmanifest",
-  "./manifest.webmanifest?v=51",
+  "./manifest.webmanifest?v=52",
   "./app-logo.png",
   "./app-logo.png?v=17",
   "./apple-touch-icon.png",
