a94f42bcb0043155aa46a9789af6f55b028d3b55
Anton A Gelman <agelman@gmail.com>
2026-07-05 11:26:49 -0400

    Add copyable attraction addresses

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const APP_VERSION = "65";
+const APP_VERSION = "66";
 const CACHE = `gelman-travel-guide-v${APP_VERSION}`;
 const FILES = [
   "./",
   "./index.html",
-  "./?v=65",
+  "./?v=66",
   "./app-version.json",
-  "./app-version.json?v=65",
+  "./app-version.json?v=66",
   "./manifest.webmanifest",
-  "./manifest.webmanifest?v=65",
+  "./manifest.webmanifest?v=66",
   "./app-logo.png",
   "./app-logo.png?v=17",
   "./apple-touch-icon.png",
