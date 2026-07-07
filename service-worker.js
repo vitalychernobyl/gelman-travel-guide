ecadaa19df2aead0d083bfe80d520a0f5ef29d38
Anton A Gelman <agelman@gmail.com>
2026-07-07 19:31:40 -0400

    Attach thumbnails to plan shares

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const APP_VERSION = "76";
+const APP_VERSION = "77";
 const CACHE = `gelman-travel-guide-v${APP_VERSION}`;
 const FILES = [
   "./",
   "./index.html",
-  "./?v=76",
+  "./?v=77",
   "./app-version.json",
-  "./app-version.json?v=76",
+  "./app-version.json?v=77",
   "./manifest.webmanifest",
-  "./manifest.webmanifest?v=76",
+  "./manifest.webmanifest?v=77",
   "./app-logo.png",
   "./app-logo.png?v=17",
   "./apple-touch-icon.png",
