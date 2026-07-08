058912c6cab901c09046305ce414a691f2dad4ff
Anton A Gelman <agelman@gmail.com>
2026-07-07 20:56:04 -0400

    Add London coffee and attraction picks

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const APP_VERSION = "78";
+const APP_VERSION = "79";
 const CACHE = `gelman-travel-guide-v${APP_VERSION}`;
 const FILES = [
   "./",
   "./index.html",
-  "./?v=78",
+  "./?v=79",
   "./app-version.json",
-  "./app-version.json?v=78",
+  "./app-version.json?v=79",
   "./manifest.webmanifest",
-  "./manifest.webmanifest?v=78",
+  "./manifest.webmanifest?v=79",
   "./app-logo.png",
   "./app-logo.png?v=17",
   "./apple-touch-icon.png",
